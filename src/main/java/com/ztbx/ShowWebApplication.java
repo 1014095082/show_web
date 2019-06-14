2ca8ad768eb130dc1635c3651911a567511bf19e
ex-cengt003 <ex-cengt003@IQSZ-D0404.cmrh.com>
2019-06-14 17:56:28 +0800

    tijiao

---
+++ src/main/java/com/ztbx/ShowWebApplication.java
@@ -0,0 +1,13 @@
+package com.ztbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowWebApplication.class, args);
+	}
+
+}
